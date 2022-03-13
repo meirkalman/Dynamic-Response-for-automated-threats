03d44b18d8da7d0e2bc8cfbde7fdc5568cf2eb4b
mk600449@gmail.com <mk600449@gmail.com>
2022-03-13 22:46:16 +0200

    add hold session logic

--- attacker.py
+++ attacker.py
@@ -3,10 +3,10 @@ import requests
 
 
 headers = {'User-Agent': 'Mozilla/5.0'}
-payload = {'Email':'dsfs@gmail.com','Password':'12345678'}
+payload = {'Email':'hiwedo2878@superyp.com','Password':'123456'}
 
 session = requests.Session()
-session.keep_alive = False
+#session.keep_alive = False
 res = session.post('http://127.0.0.1:5000/Account/Login',data=payload,headers = headers)
 
 
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import simplejson
 import time
+from requests import post
+
 from fake_responses import *
 import requests
 from flask import Flask, request, Response
 
 app = Flask('__main__')
-SITE_NAME = 'http://localhost:3000/'
+SITE_NAME = 'https://hack-yourself-first.com/'
 
 
 def detect_attack():
@@ -13,7 +15,7 @@ def detect_attack():
 
 
 @app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+#@app.route('/<path:path>')
 def proxy(path):
     # s = requests.session()
     # if detect_attack():
@@ -35,10 +37,23 @@ def proxy(path):
     excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
     headers = [(name, value) for (name, value) in resp.raw.headers.items()
                if name.lower() not in excluded_headers]
-
     response = Response(resp.content, resp.status_code, headers)
 
     return response
+
+
+
+@app.route('/Account/Login',methods = ['POST','GET'])
+def login():
+    payload = request.get_data(as_text=True)
+    print(payload)
+    pay2 = payload.split("&")
+    email = (pay2[0].split("="))[1].replace("%40","@")
+    print(email)
+    pass1 = (pay2[1].split("="))[1]
+    print(pass1)
+    payload = {'Email': email, 'Password': pass1}
+    return post(f'{SITE_NAME}/Account/Login',data=payload).content
 
 
 app.run(debug=True)
